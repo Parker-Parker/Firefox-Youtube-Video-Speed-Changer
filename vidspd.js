f34795096045ab2a055c6279091dbceb27a1ebc9
Parker-Parker <pagrant@wpi.edu>
2023-12-22 19:01:22 -0500

    added storage listener

--- vidspd.js
+++ vidspd.js
@@ -1,13 +1,11 @@
 console.log("Wideo Speed plugin loaded: started")
 // Select the node that will be observed for mutations
-// const targetNode = document.querySelector('title');;
-// const targetNode = document.getElementsByTagName("video")[0].playbackRate;
 const targetNode = document.getElementsByTagName("video")[0];
 
 // Options for the observer (which mutations to observe)
-// const config = {childList: true};
-// const config = { attributes: true, childList: true, subtree: true };
 const config = { attributes: true};
+
+//TODO: may need to add 2nd observer that checks if document.getElementsByTagName("video")[0]'s parent childList has changed, then remake mutationObserver for document.getElementsByTagName("video")[0].playbackSpeed
 
 // Video Speed
 var videoSpeed = browser.storage.local.get("videoSpeed");
@@ -16,16 +14,14 @@ if (!(typeof videoSpeed === 'number')){
   videoSpeed = 2.0;
 }
 
+// set page's video speed to desired speed
 function syncVidSpeed() {
 
   if( !(targetNode.playbackRate === videoSpeed)  )  {
     targetNode.playbackRate = videoSpeed;
-    console.log("Speed set:"+videoSpeed )
+    console.log("Speed set:"+videoSpeed );
   }  
 
-  // if( !(document.getElementsByTagName("video")[0].playbackRate === videoSpeed)  )  {
-  //   document.getElementsByTagName("video")[0].playbackRate = videoSpeed;
-  // }  
 }
 
 // Callback function to execute when mutations are observed
@@ -37,12 +33,34 @@ const mutationCallback = (mutationList, observer) => {
   }
 };
 
+
+// Callback function to execute when stored Speed has changed
+const storedSpeedCallback = (changes, area) => {
+  if (area === "local") {
+    if("videoSpeed" in changes){
+      if(  !(changes["videoSpeed"].newValue === videoSpeed)  ){
+        videoSpeed = changes["videoSpeed"].newValue;
+        console.log("New Speed from storage: "+videoSpeed);
+        syncVidSpeed();
+      }
+    }
+  } 
+
+};
+
+
+
 // Create an observer instance linked to the callback function
 const mutationObserver = new MutationObserver(mutationCallback);
 
-
-
 // Start observing the target node for configured mutations
 mutationObserver.observe(targetNode, config);
+
+// Start listening to the stored target speed
+browser.storage.onChanged.addListener(storedSpeedCallback);
+
+
 console.log("Wideo Speed plugin loaded: Live")
+
+
 
