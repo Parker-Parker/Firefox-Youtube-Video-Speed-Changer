f6003c8399811c5fb18845ae2a882f9291d1f73c
Parker-Parker <pagrant@wpi.edu>
2023-12-22 22:00:22 -0500

    javascript is garbage async/await is garbage promises are garbage

--- popup/popup.js
+++ popup/popup.js
@@ -1,54 +1,63 @@
-/**
-* Listen for clicks on the buttons, and send the appropriate message to
-* the content script in the page.
-*/
-console.log("Popup began");
 
-console.log(browser.storage.local.get("videoSpeed"));
+async function runthing() {
 
-var videoSpeed = browser.storage.local.get("videoSpeed").then();
-if (!(typeof videoSpeed === 'number')){
-  console.log("Initializing storage to 3");
-  browser.storage.local.set({"videoSpeed":3.0});
-  videoSpeed = 3.0;
-}
+    /**
+    * Listen for clicks on the buttons, and send the appropriate message to
+    * the content script in the page.
+    */
+    console.log("Popup began");
+    var videoSpeed = 1.0;
+    // browser.storage.local.get("videoSpeed").then();
+    var result = await browser.storage.local.get("videoSpeed");
+    videoSpeed = result.videoSpeed;
 
-// document.getElementById("speed-input").setAttribute("value", videoSpeed)
+    console.log(videoSpeed);
 
-const input = document.getElementById("speed-input");
-input.setAttribute("value", videoSpeed)
 
-function listenForClicks() {
+    if (!(typeof videoSpeed === 'number')){
+    console.log("Initializing storage to 3");
+    browser.storage.local.set({"videoSpeed":3.0});
+    videoSpeed = 3.0;
+    }
 
-    console.log("Registering listener");
-    document.addEventListener("click", (e) => {
-        console.log("Listener Triggered"+input.value);
+    // document.getElementById("speed-input").setAttribute("value", videoSpeed)
 
-        if (e.target.tagName !== "BUTTON" || !e.target.closest("#popup-content")) {
-            // Ignore when click is not on a button within <div id="popup-content">.
-            return;
-        } else {
-            // videoSpeed = Number(document.getElementById("speed-input").getAttribute("value"));
-            videoSpeed = Number(input.value);
-            if(!isNaN(videoSpeed)){
-                console.log("setting video speed: "+videoSpeed);
-                browser.storage.local.set({"videoSpeed":videoSpeed});
+    const input = document.getElementById("speed-input");
+    input.setAttribute("value", videoSpeed)
+
+    function listenForClicks() {
+
+        console.log("Registering listener");
+        document.addEventListener("click", (e) => {
+            console.log("Listener Triggered"+input.value);
+
+            if (e.target.tagName !== "BUTTON" || !e.target.closest("#popup-content")) {
+                // Ignore when click is not on a button within <div id="popup-content">.
+                return;
+            } else {
+                // videoSpeed = Number(document.getElementById("speed-input").getAttribute("value"));
+                videoSpeed = Number(input.value);
+                if(!isNaN(videoSpeed)){
+                    console.log("setting video speed: "+videoSpeed);
+                    browser.storage.local.set({"videoSpeed":videoSpeed});
+                }
+                else {
+                    console.log("NaN, Speed not stored");
+                }
+
+                //code here
+
             }
-            else {
-                console.log("NaN, Speed not stored");
-            }
+            console.log("Listener completed");
+        });
+        console.log("Listener Registered");
+    }
 
-            //code here
 
-        }
-        console.log("Listener completed");
-    });
-    console.log("Listener Registered");
+    // start the button click listener
+    listenForClicks();
+
+    console.log("Popup finn");
+
 }
-
-
-// start the button click listener
-listenForClicks();
-
-console.log("Popup finn");
-
+runthing();
